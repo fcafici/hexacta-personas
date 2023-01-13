39cf695803339aeb21dc6039472d1f5788e67c6b
fcafici <fcafici@frba.utn.edu.ar>
2023-01-13 16:24:45 -0300

    Ejercicio completo funcionando Falta responder preguntas y optimizar

--- ProyectoHexactaUI/src/app/app.module.ts
+++ ProyectoHexactaUI/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +18,8 @@ import { IngresarPersonaComponent } from './components/ingresar-persona/ingresar
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProyectoHexactaUI/src/app/components/ingresar-persona/ingresar-persona.component.ts
+++ ProyectoHexactaUI/src/app/components/ingresar-persona/ingresar-persona.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Persona } from 'src/app/models/persona.model';
 import { PersonasService } from 'src/app/services/personas.service';
 
@@ -8,7 +10,14 @@ import { PersonasService } from 'src/app/services/personas.service';
   styleUrls: ['./ingresar-persona.component.css']
 })
 export class IngresarPersonaComponent implements OnInit {
-
+/*
+  formIngreso: FormGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      dni: new FormControl('', [Validators.required, Validators.maxLength(8), Validators.min(1)]),
+      fechaNacimiento: new FormControl('', [Validators.required])
+  });
+*/
   agregarPersonaRequest: Persona = {
     nombre: '',
     apellido: '',
@@ -20,24 +29,64 @@ export class IngresarPersonaComponent implements OnInit {
   };
 
   fechaNacimientoString: string = '';
-  fechaNacimiento: Date = new Date();
 
-constructor(private personaService: PersonasService) {
+  // estaría bueno hacer una lista con estas validaciones y recorrerla para validar
+  // voy a ver que ande así y después intentar mejorarlo.
+  nombreValido: boolean = false;
+  apellidoValido: boolean = false;
+  dniValido: boolean = false;
+  fechaNacimientoValida: boolean = false;
+
+constructor(private personaService: PersonasService, private router: Router) {
 
 }
 
+validarLongitud(atributo : string) : boolean {
+  return atributo.length >= 3 && atributo.length <= 50;
+} 
+
+validarDni(dni: number) : boolean {
+  return dni > 0 && dni <= 99999999;
+}
+
+validarFechaPasada(fechaString : string) : boolean {
+  console.log("entre a validar fecha pasada");
+  var fecha: Date = new Date(fechaString);
+  return fecha < new Date();  // new Date() es la fecha actual
+}
+
+/*
+  initFormPersona(): void {
+    this.formIngreso = new FormGroup({
+      
+    });
+  }
+*/
   ngOnInit(): void {
+    //this.initFormPersona();
+  }
+
+  generarFechaNacimiento(persona: Persona, fechaNacimientoString: string): void {
+    var fecha: Date = new Date(fechaNacimientoString);
+    persona.diaNacimiento = fecha.getDate() + 1;  // +1 porque el 0 es el primer dia del mes
+    persona.mesNacimiento = fecha.getMonth() + 1; // +1 porque el 0 es el primer mes del año
+    persona.anioNacimiento = fecha.getFullYear();
+  }
+
+  validRequest() : boolean {
+    // lo correcto sería que las validaciones sean parte de una lista y hacer un validaciones.all({v => v == true}) o algo por el estilo
+    return this.nombreValido && this.apellidoValido && this.dniValido && this.fechaNacimientoValida;
   }
 
   agregarPersona() {
-    this.fechaNacimiento = new Date(this.fechaNacimientoString);
-    this.agregarPersonaRequest.diaNacimiento = this.fechaNacimiento.getDate() + 1;  // +1 porque el 0 es el primer dia del mes
-    this.agregarPersonaRequest.mesNacimiento = this.fechaNacimiento.getMonth() + 1; // +1 porque el 0 es el primer mes del año
-    this.agregarPersonaRequest.anioNacimiento = this.fechaNacimiento.getFullYear();
+    this.generarFechaNacimiento(this.agregarPersonaRequest, this.fechaNacimientoString)
+    
+    if (!this.validRequest()) return;
 
     this.personaService.agregarPersona(this.agregarPersonaRequest).subscribe({
       next: (persona) => {
         console.log(persona);
+        this.router.navigate(['/personas']);
       },
       error: (error) => {
         console.log(error);
